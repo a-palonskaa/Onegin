24fded11c6b5fd260676e5906a8925eeb1797814
Alina <alinapalonskaa0@gmail.com>
2024-09-08 14:07:18 +0300

    skip symbols

--- string_functions.cpp
+++ string_functions.cpp
@@ -3,15 +3,32 @@
 
 #include "string_functions.h"
 
+static void SkipNonAlphaSymbols(int* i, int*j, const char* string1, const char* string2)
+
 bool CompareStr(char* string1, char* string2) {
     assert(string1 != nullptr);
     assert(string2 != nullptr);
 
     int i = 0;
+    int j = 0;
 
-    for (; i < MAX_STR_LEN && string1[i] != '\0' && tolower(string1[i]) == tolower(string2[i]); i++) {
-        ;
+    while(!isalpha(string1[i])) {
+        i++;
     }
+    while(!isalpha(string2[j])) {
+        j++;
+    }
+
+    for(;i < MAX_STR_LEN && j < MAX_STR_LEN &&
+        string1[i] != '\0' && tolower(string1[i]) == tolower(string2[j]); i++, j++) {
+        while(!isalpha(string1[i]) && i < MAX_STR_LEN && string1[i] != '\0') {
+            i++;
+        }
+        while(!isalpha(string2[j]) && j < MAX_STR_LEN && string1[j] != '\0') {
+            j++;
+        }
+    }
+
     return (string1[i] > string2[i]);
 }
 
@@ -25,5 +42,29 @@ void SwapStr(char* string1, char* string2) {
         c = string1[i];
         string1[i] = string2[i];
         string2[i] = c;
+    }
+}
+
+
+    // do {
+    //     while(!isalpha(string1[i])) {
+    //         printf("symbol %c is not alpha\n", string1[i]);
+    //         i++;
+    //     }
+    //     while(!isalpha(string2[j])) {
+    //         j++;
+    //         printf("symbol %c is not alpha\n", string2[j]);
+    //     }
+    //     printf("i = %d, j = %d\n", i, j);
+    // } while (i < MAX_STR_LEN && j < MAX_STR_LEN &&
+    //          string1[i] != '\0' &&
+    //          tolower(string1[i]) == tolower(string2[j]));
+
+static void SkipNonAlphaSymbols(int* i, int*j, const char* string1, const char* string2) {
+    while(!isalpha(string1[*i]) && *i < MAX_STR_LEN && string1[*i] != '\0') {
+        *i++;
+    }
+    while(!isalpha(string2[*j]) && *j < MAX_STR_LEN && string1[*j] != '\0') {
+        *j++;
     }
 }
--- version2.cpp
+++ version2.cpp
@@ -4,14 +4,14 @@
 #include "string_functions.h"
 
 int main() {
-    FILE* input_file = fopen("onegin.txt", "r");
+    FILE* input_file = fopen("testingdata.txt", "r");
 
     char text[STR_CNT][MAX_STR_LEN] = {};
 
     for (int i = 0; i < STR_CNT; i++) {
         fgets(&text[i][0],MAX_STR_LEN, input_file);
     }
-
+    PrintSymbolsTestMode(text);
     for (int i = 0; i < STR_CNT; i++) {
         for (int j = i + 1; j < STR_CNT; j++) {
             if (CompareStr(text[i], text[j])) {
